e7bcb63e5f40a20a0ac9a4ce83f6f1301803db8e
chriszeng8 <chriszeng8@hotmail.com>
2015-01-16 22:58:38 -0500

    Useful R commands

--- Useful_Scripts_and_Tips/Useful_R_commands.r
+++ Useful_Scripts_and_Tips/Useful_R_commands.r
@@ -6,12 +6,20 @@ cat("\014")
 # 2. Clean the workspace: remove all the variables stored in workspace
 rm(list=ls())
 
-# 3. Str (structure) function 
+#3. get the quick argument list of a function
+arg(lm)
+
+# 4. Str (structure) function 
 # a. str can give brief summary of a function
 str(lm)
 # b. str can also quickly give a short summary output of any R object.
 x<-list(a=1:4,b=8:12,c=16:20)
 str(x)
 
-# 4. list all objects/variables in your local workspace.
+# 5. list all objects/variables in your local workspace.
 ls()
+
+# 6. Table function for counting the frequency of each factor level
+y<-rbinom(10,1,0.5)
+as.data.frame(table(y))
+
