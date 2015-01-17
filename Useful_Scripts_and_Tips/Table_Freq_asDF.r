66f67bab0a1370d61d57dc81e6cdbd22f1f9286a
chriszeng8 <chriszeng8@hotmail.com>
2015-01-17 02:10:42 -0500

    table function and data table .N function datatable_a[,.N, by=y] to count frequency of factor levels

--- Useful_Scripts_and_Tips/Table_Freq_asDF.r
+++ Useful_Scripts_and_Tips/Table_Freq_asDF.r
@@ -4,7 +4,7 @@
 # Combined with as.data.frame(table(DF$y))
 
 # Example 1
-DF<-data.frame(x=1:6,y=rep(letters[1:3],each=2),z=3)
+DF<-data.frame(x=1:6,y=c("a","b","b","b","c","c"),z=3)
 AllLetters<-DF$y
 as.data.frame(table(AllLetters))
 
@@ -12,3 +12,11 @@ as.data.frame(table(AllLetters))
 Coin_Records<-rbinom(10,1,0.5)
 as.data.frame(table(Coin_Records))
 
+# Alternatively, data.table is an alternative way
+# Example 1
+library(data.table)
+DT<-data.table(x=1:6,y=c("a","b","b","b","c","c"),z=3)
+DT[,.N,by=y]
+
+# More on differences between data table and data frame.
+# http://stackoverflow.com/questions/13618488/what-you-can-do-with-data-frame-that-you-cant-in-data-table
