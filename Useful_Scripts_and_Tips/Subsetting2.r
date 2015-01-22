7018ccf6f17dc6c88c7e7e8c81843f5378909b11
chriszeng8 <chriszeng8@hotmail.com>
2015-01-21 23:59:21 -0500

    Subsetting using which(),%in% and ifelse() functions

--- Useful_Scripts_and_Tips/Subsetting2.r
+++ Useful_Scripts_and_Tips/Subsetting2.r
@@ -1,7 +1,13 @@
-# Using which function to handle the missing values
+# Jan 20 2015, C.Z.Zeng 
+# ================= 1. which() function is great in handling the missing values ========================
+# ================= 2. %in % is a great replacement for equality (=) sign,      ========================
+# =================    and can be applied to multiple criteria.                 ========================
+# ================= 3. ifselse() function to create binary similar functions    ========================
 
-x<-data.frame(var1=sample(1:5),var2=sample(6:10))
-x[c(1,3),"var2"]=NA
+
+# Create a data frame
+set.seed(1)
+x<-data.frame(var1=sample(1:5),var2=sample(6:10));x[c(1,3),"var2"]=NA
 
 # Simply logical statement on a column with missing value (NA), will not enable us to subset
 Subset_logical<-x$var2>8
@@ -10,7 +16,17 @@ Subset_logical<-x$var2>8
 x[Subset_logical,] # or x[x$var2>8]
 # which will include the missing values
 
-
 # The which() function returns the indices of none-missing values that satisfy the criteria
 x[which(x$var2>6),]
+
+# =========================== %in% =============================
+x[x$var2 %in% 6:10,]
+# one great application of %in% is to combine with c(a,b) to define
+# multiple discrete equality criteria
+x[x$var2 %in% c(5,7,9),]
+
+
+# ========= creating ifelse variables ============
+l_or_g<-ifelse(x$var2<6,"less","greater")
+table(l_or_g)
 
