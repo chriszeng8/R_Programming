cf7364b8c4dcf05a824acbf419842ddd8bfbb5b4
chriszeng8 <chriszeng8@hotmail.com>
2015-02-10 02:36:15 -0500

    subset using textual vs numeric key after applying setkey()

--- Useful_Scripts_and_Tips/data_table_keys.r
+++ Useful_Scripts_and_Tips/data_table_keys.r
@@ -2,12 +2,25 @@
 library(data.table)
 
 # Example 1: Use keys to subset tables
+# Query group based on "numeric" key
 # Create a data table
-DT<-data.table(x=rep(c("a","b","c"),each=100),y=rnorm(300))
+DT<-data.table(x=rep(c(3,2,1),each=100),y=rnorm(300))
 # Set x as the key column (like SQL)
 setkey(DT,x)
+# Find all data records with 1 in the key column
+DT[.(1)] # if key is numeric value
+DT[list(1)] # if key is a numeric value
+# Not to confused with DT[1] which own subset the first row
+
+
+# Example 2: Use keys to subset tables
+# Query group based on "textual" key
+DT2<-data.table(x=rep(c("a","b","c"),each=100),y=rnorm(300))
+# Set x as the key column (like SQL)
+setkey(DT2,x)
 # Find all data records with 'a' in the key column
-DT['b']
+DT2['a']
+
 
 # Example 2: Use keys to merge 
 # (Note that key does not need to be unique ID, but in the following example, it makes
